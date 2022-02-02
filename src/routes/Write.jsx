8bdf877995fbdfbbccb1621634ce0d44b62c8ad2
vanillovin <vanillovin@gmail.com>
2022-02-02 15:25:33 +0900

    textarea를 벗어나고,, toast-ui editor 적용

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, createRef, useEffect } from 'react';
 import { Redirect, useHistory, withRouter } from 'react-router';
 import styled from 'styled-components';
 import { useT, useUser } from '../context';
 import { boardApi } from '../api';
 
+import Prism from 'prismjs';
+// 여기 css를 수정해서 코드 하이라이팅 커스텀 가능
+import 'prismjs/themes/prism.css';
+
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor, Viewer } from '@toast-ui/react-editor';
+
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+
 const Container = styled.div`
-  width: 700px;
+  width: 750px;
 `;
 const Title = styled.h1`
   font-size: 24px;
@@ -16,7 +27,7 @@ const Select = styled.select`
   width: 100%;
   padding: 10px;
   outline: none;
-  margin-bottom: 5px;
+  margin-bottom: 6px;
   border: 1px solid lightgray;
 `;
 const TitleInput = styled.input`
@@ -26,14 +37,10 @@ const TitleInput = styled.input`
   margin-bottom: 10px;
   border: 1px solid lightgray;
 `;
-const ContentInput = styled.textarea`
+const EditorContainer = styled.div`
   width: 100%;
-  height: 300px;
-  outline: none;
-  padding: 10px 10px 10px 12px;
-  border: 1px solid lightgray;
-  resize: none;
-  margin-bottom: 10px;
+  margin-bottom: 15px;
+  background-color: white;
 `;
 const ButtonContainer = styled.div`
   display: flex;
@@ -54,14 +61,19 @@ const Button = styled.button`
   transition: all 0.1s linear;
 `;
 
+// Viewer 사용
+function PostView() {
+  return <Viewer plugins={[[codeSyntaxHighlight, { highlighter: Prism }]]} />;
+}
+
+// Editor 사용
 const Write = ({
   match,
   location: {
     state: { type: bType },
   },
 }) => {
-  console.log(match, bType);
-
+  //   console.log(match, bType);
   const t = useT();
   const history = useHistory();
   const loggedIn = Boolean(useUser());
@@ -83,41 +95,50 @@ const Write = ({
 
   const onSubmit = (e) => {
     // e.preventDefault();
-    // console.log('Write onSubmit', type, title, content);
+    const content = editorRef.current.getInstance().getMarkdown();
+    console.log('Write onSubmit', type, title, content, content.length);
 
-    if (type === '') {
-      alert('게시판을 선택해 주세요');
-      return;
-    }
-    if (title.trim() === '' || title.trim().length < 2) {
-      alert('제목은 2자 이상 입력해 주세요');
-      return;
-    }
-    if (content.trim().length < 4) {
-      // byte
-      alert('내용은 4자 이상 입력해 주세요');
-      return;
+    // if (type === '') {
+    //   alert('게시판을 선택해 주세요');
+    //   return;
+    // }
+    // if (title.trim() === '' || title.trim().length < 2) {
+    //   alert('제목은 2자 이상 입력해 주세요');
+    //   return;
+    // }
+    const instance = editorRef.current.getInstance();
+    const data = instance.getMarkdown();
+
+    if (!data) {
+      // ...
+      instance.focus(); // ERROR
     }
 
-    const data = {
-      title,
-      content,
-    };
+    // if (content.trim().length < 4) {
+    //   // byte
+    //   alert('내용은 4자 이상 입력해 주세요');
+    //   return;
+    // }
 
-    const ok = window.confirm('게시물을 등록하시겠습니까?');
-    ok &&
-      boardApi
-        .sendPost(type, data, t)
-        .then((res) => {
-          console.log('sendPost res =>', res);
-          const id = res.data.id;
-          history.push(`/board/${type}/${id}`);
-        })
-        .catch((err) => {
-          console.log('sendPost err =>', err || err.response?.status);
-          // localStorage.removeItem('user');
-          // window.location.href = '/'
-        });
+    // const data = {
+    //   title,
+    //   content: editorRef.current.getInstance().getMarkdown(),
+    // };
+
+    // const ok = window.confirm('게시물을 등록하시겠습니까?');
+    // ok &&
+    //   boardApi
+    //     .sendPost(type, data, t)
+    //     .then((res) => {
+    //       console.log('sendPost res =>', res);
+    //       const id = res.data.id;
+    //       history.push(`/board/${type}/${id}`);
+    //     })
+    //     .catch((err) => {
+    //       console.log('sendPost err =>', err || err.response?.status);
+    //       // localStorage.removeItem('user');
+    //       // window.location.href = '/'
+    //     });
   };
 
   const cancelWrite = () => {
@@ -129,6 +150,20 @@ const Write = ({
   if (type === 'qna') typeText = 'Q&A';
   if (type === 'tech') typeText = 'Tech';
   if (type === 'free') typeText = '자유게시판';
+
+  const editorRef = createRef();
+
+  const onChangeEditorTextHandler = () => {
+    console.log(editorRef.current.getInstance().getMarkdown());
+    // console.log(editorRef.current);
+  };
+
+  useEffect(() => {
+    console.log(editorRef.current.getInstance());
+    console.log(editorRef.current.getInstance());
+    editorRef.current.getInstance().focus();
+    // console.log(editorRef.current.getInstance().getCurrentModeEditor().focus);
+  }, []);
 
   return (
     <>
@@ -152,12 +187,21 @@ const Write = ({
             required
           />
 
-          <ContentInput
-            name="content"
-            value={content}
-            onChange={onChange}
-            required
-          />
+          <EditorContainer>
+            <Editor
+              initialValue="마크다운으로 내용을 입력하세요."
+              previewStyle="vertical"
+              initalEditType="markdown"
+              plugins={[
+                colorSyntax,
+                [codeSyntaxHighlight, { highlighter: Prism }],
+              ]}
+              ref={editorRef}
+              onChange={onChangeEditorTextHandler}
+              height="600px"
+              useCommandShortcut={true}
+            />
+          </EditorContainer>
 
           <ButtonContainer>
             <Button onClick={cancelWrite}>취소하기</Button>
