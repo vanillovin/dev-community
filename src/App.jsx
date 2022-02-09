5add36ee531f62a324e596276475b07301fb21a4
vanillovin <vanillovin@gmail.com>
2022-02-10 05:47:20 +0900

    ReactQueryDevtools 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Router from './components/Router';
 import GlobalStyles from './components/GlobalStyles';
 import { UserProvider } from './context';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ function App() {
         <GlobalStyles />
         <Router />
       </UserProvider>
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
