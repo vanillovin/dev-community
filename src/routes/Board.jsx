a4e1962167d76e2a2a455a624706d051821745f0
vanillovin <vanillovin@gmail.com>
2022-02-10 05:46:58 +0900

    검색 기능 부분 제외 react-query 적용

--- src/routes/Board.jsx
+++ src/routes/Board.jsx
@@ -6,6 +6,7 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import { boardApi } from '../api';
 import PageList from '../components/PageList';
 import { useUser } from '../context';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 
 const Container = styled.div`
   width: 750px;
@@ -121,100 +122,131 @@ const Board = () => {
     (boardType === 'free' && '자유게시판');
   // const sort = location.pathname.split('=')[1] || location.state?.sort || null;
 
-  const [boards, setBoards] = useState({
-    sort: '',
-    isLoading: false,
-    contents: null,
-    currentPage: 1,
-    totalElements: null,
-    totalPages: null,
-    index: 'title',
-    keyword: '',
-  });
-  const {
-    sort,
-    isLoading,
-    contents,
-    currentPage,
-    totalElements,
-    totalPages,
-    index,
-    keyword,
-  } = boards;
-  const [indexInput, setIndexInput] = useState('title'); // searchMode
-  const [keywordInput, setKeywordInput] = useState('');
-  const [pageList, setPageList] = useState([1, 2, 3, 4, 5]);
-
   const history = useHistory();
 
-  // https://developer.mozilla.org/ko/docs/Web/API/URLSearchParams
-  // location.search; // /index=1&keyword=토끼
+  const [sort, setSort] = useState('createdDate');
+  const [curPage, setCurPage] = useState(1);
+  const [pageList, setPageList] = useState([1, 2, 3, 4, 5]);
 
-  // 주소창과 상태를 동기화
-  // 상태에 따라
-  useEffect(() => {
-    const searchParams = new URLSearchParams();
-    searchParams.set('index', index);
-    searchParams.set('keyword', keyword);
-    console.log('searchParams', searchParams.toString());
+  const [searchModeInput, setSearchModeInput] = useState('title'); // searchModeInput
+  const [keywordInput, setKeywordInput] = useState('');
+  const [searchInfo, setSearchInfo] = useState({
+    keyword: '',
+    searchMode: '',
+  });
+  const { keyword, searchMode } = searchInfo;
 
-    history.push(`/board/${boardType}?` + searchParams.toString());
-  }, [index, keyword]);
+  const { isLoading, data } = useQuery(
+    [`${boardType}Board`, curPage, sort],
+    () => boardApi.getPosts(curPage, sort, boardType).then((res) => res.data)
+  );
 
-  // 검색어는 어딘가에서 유지되야 함=> keyword, index 상태를 boards 에 추가!
-  // 주소창 search params로 현재 검색중인 검색어를 동기화 갱신, useEffect (주소창 갱신)
-  // searchPosts를 이용해서... index랑 keyword 바뀔 때마다, if문으로 데이터 불러오기
+  // # 조건? 과 요구사항
+  // 검색 결과 boards와 type별(qna, tech, free) boards의 api 요청 주소가 다름.
+  // queryParams? url은 건들지 않고, 일단 위에 [`${boardType}Board`, ...] <- 이 키의 data를
+  // 검색 버튼을 눌렀을 때 실행되는 handleSearch 함수를 호출해서 검색 api data로 결과로 바꾸고 싶음.
+  //
+  // -문제점
+  //   -검색어 input을 입력할 때마다 위의 const { isLoading, data } = useQuery(
+  //    [`${boardType}Board`, curPage, sort], () => board; ... 데이터가 계속 요청됨.
+  //   -몇 초 뒤에 mutate로 호출한 값이 사라지고 불규칙하게? 다시 위에 data가 재요청돼서
+  //    검색결과 data가 사라진다.
+  //
+  // -궁금한 점
+  //   -onSuccess에서 result 값은 위에서 요청한 url 값이 맞는가?
+  //   -그리고 pagenation 인가,, state가 바뀔 때마다 재요청 되도록 query key 배열 안에
+  //    state들을 넣어놨는데, 그럼 mutate나 데이터를 건드는 작업을 할 때
+  //    [`${a}b`] 이렇게만 입력했는데 정상적으로 결과를 받았는데
+  //    [`${a}b`, state, state] 이런 식으로 초반에 입력된 키와 똑같이 입력하는지,,
+  //
+  // 너무 어렵다. react-query에 대한 자료도 별로 없고 문서도 복잡하다ㅜㅜ
 
-  const onSubmit = (e) => {
-    console.log(indexInput, '/', keywordInput);
-
-    e.preventDefault();
-    if (keywordInput.trim().length < 2) return;
-
-    setBoards((prev) => ({
-      ...prev,
-      keyword: keywordInput,
-      index: indexInput,
-    }));
-  };
-
-  // 검색어를 입력하고 ENTER키나 검색버튼을 누르면 searchPosts api를 실행해서 boardsState 변경
-  // pagelist에서 다른 번호를 눌러도 위의 조건이 적용되어야함.
-  const fetchBoardData = (page, sort = 'createdDate') => {
-    boardApi
-      .getPosts(page, sort, boardType)
-      .then((res) => {
-        console.log('fetchBoardData res =>', res.data);
-        setBoards((prev) => ({
-          ...prev,
-          sort,
-          ...res.data,
-        }));
-      })
-      .catch(console.error);
-  };
-
-  // ALL, CONTENT, ERROR, TITLE
-  const fetchSearchBoardData = (keyword, page, index) => {
-    boardApi
-      .searchPosts(keyword, page, index, boardType)
-      .then((res) => {
-        setBoards((prev) => ({
-          ...prev,
-          sort,
-          ...res.data,
-        }));
-      })
-      .catch(console.error);
-  };
-
-  useEffect(() => {
-    if (keyword === '') {
-      fetchBoardData(1);
-    } else {
-      fetchSearchBoardData(keyword, 1, index);
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(
+    () =>
+      boardApi
+        .searchPosts(keyword, curPage, searchMode, boardType)
+        .then((res) => res.data),
+    {
+      // onMutate: () => {
+      //   const previousValue = queryClient.getQueryData([`${boardType}Board`]);
+      //   console.log('previousValue', data);
+      //   queryClient.setQueryData([`${boardType}Board`], (old) => {
+      //     console.log('old', old);
+      //     return [old, data];
+      //   });
+      //   return previousValue;
+      // },
+      onSuccess: (result) => {
+        console.log('성공 메시지:', result);
+        queryClient.setQueriesData([`${boardType}Board`], result);
+      },
     }
-  }, [boardType, keyword, index]);
+  );
+
+  console.log('Board react-query data', data);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (keywordInput.trim().length < 2) {
+      alert('검색어는 2글자 이상 입력해주세요.');
+      return;
+    }
+    setSearchInfo({
+      keyword: keywordInput,
+      searchMode: searchModeInput,
+    });
+    mutate();
+    // mutate();
+  };
+
+  // useEffect(() => {
+  //   const searchParams = new URLSearchParams();
+  //   searchParams.set('searchMode', searchMode);
+  //   searchParams.set('keyword', keyword);
+  //   console.log('searchParams', searchParams.toString());
+
+  //   history.push(`/board/${boardType}?` + searchParams.toString());
+  // }, [searchMode, keyword]);
+
+  // const { mutate } = useMutation(
+  //   () =>
+  //     boardApi
+  //       .searchPosts(keyword, curPage, searchMode, boardType)
+  //       .then((res) => res.data),
+  //   {
+  //     onSuccess: () => {
+  //       refetch();
+  //       queryClient.invalidateQueries([
+  //         `${boardType}Board`,
+  //         curPage,
+  //         sort,
+  //         searchInfo,
+  //       ]);
+  //     },
+  //   }
+  // );
+
+  // const fetchSearchBoardData = (keyword, page, searchMode) => {
+  //   boardApi
+  //     .searchPosts(keyword, page, searchMode, boardType)
+  //     .then((res) => {
+  //       setBoards((prev) => ({
+  //         ...prev,
+  //         sort,
+  //         ...res.data,
+  //       }));
+  //     })
+  //     .catch(console.error);
+  // };
+
+  // useEffect(() => {
+  //   if (keyword === '') {
+  //     fetchBoardData(1);
+  //   } else {
+  //     fetchSearchBoardData(keyword, 1, searchMode);
+  //   }
+  // }, [boardType, keyword, searchMode]);
 
   const orderListClick = (e) => {
     const innerText = e.target.innerText;
@@ -222,7 +254,8 @@ const Board = () => {
     if (innerText === '조회순') sort = 'views';
     if (innerText === '좋아요순') sort = 'likes';
     if (innerText === '댓글순') sort = 'commentSize';
-    fetchBoardData(1, sort);
+    setCurPage(1);
+    setSort(sort);
   };
 
   return (
@@ -243,15 +276,14 @@ const Board = () => {
 
       <FilterContainer>
         <OrderList onClick={orderListClick}>
-          {console.log('sort', sort)}
           <OrderItem active={sort === 'createdDate'}>최신순</OrderItem>
           <OrderItem active={sort === 'views'}>조회순</OrderItem>
           <OrderItem active={sort === 'commentSize'}>댓글순</OrderItem>
           <OrderItem active={sort === 'likes'}>좋아요순</OrderItem>
         </OrderList>
 
-        <SearchForm onSubmit={onSubmit}>
-          <SearchSelect onChange={(e) => setIndexInput(e.target.value)}>
+        <SearchForm handleSearch={handleSearch}>
+          <SearchSelect onChange={(e) => setSearchModeInput(e.target.value)}>
             <option value="title" defaultValue>
               제목
             </option>
@@ -264,7 +296,7 @@ const Board = () => {
             minLength="2"
             maxLength="20"
           />
-          <SearchButton type="submit" onClick={onSubmit}>
+          <SearchButton type="submit" onClick={handleSearch}>
             <AiOutlineSearch className="sbtn" />
           </SearchButton>
         </SearchForm>
@@ -272,28 +304,14 @@ const Board = () => {
 
       {!isLoading ? (
         <>
-          {contents?.map((post) => (
+          {data?.contents?.map((post) => (
             <Post
               key={post.id}
               post={post}
               type={boardType}
-              fci={contents[0].id}
+              fci={data?.contents[0].id}
             />
           ))}
-          {/* {totalPages > 0 ? (
-            <PageList
-              page={pageList}
-              setPage={setPageList}
-              fetchContents={fetchBoardData}
-              totalPages={totalPages}
-              currentPage={currentPage}
-              sort={sort}
-            />
-          ) : (
-            <div style={{ marginTop: 150, textAlign: 'center' }}>
-              아직 게시물이 없습니다.
-            </div>
-          )} */}
         </>
       ) : (
         <div>loading..</div>
