50cb564b2013b0a945c097000c4a6d07237bdc44
vanillovin <vanillovin@gmail.com>
2022-02-14 16:03:04 +0900

    미엘님이 알려주신 부분 수정 전

--- src/routes/Board.jsx
+++ src/routes/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Link, useLocation, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { AiOutlineSearch } from 'react-icons/ai';
@@ -142,7 +142,7 @@ const CancelButton = styled.button`
 const Board = () => {
   const user = useUser();
   const location = useLocation();
-  const boardType = location.pathname.split('/')[2] || location.state?.type;
+  const boardType = location.state?.type || location.pathname.split('/')[2];
   const boardTitle =
     (boardType === 'qna' && 'Q&A') ||
     (boardType === 'tech' && 'Tech') ||
@@ -150,31 +150,16 @@ const Board = () => {
 
   const history = useHistory();
 
-  const [sort, setSort] = useState('createdDate');
-
   const search = new URLSearchParams(location.search);
-  // const sort = search.get('sort');
+  const sort = search.get('sort') || 'createdDate';
   const curPage = Number(search.get('page')) || 1;
-  let x = curPage;
-  if (curPage % 5 === 1) x += 4;
-  if (curPage % 5 === 2) x += 3;
-  if (curPage % 5 === 3) x += 2;
-  if (curPage % 5 === 4) x += 1;
-  const initPageList =
-    curPage > 5
-      ? [1, 2, 3, 4, 5].map((num) => num + 5 * (x / 5 - 1))
-      : [1, 2, 3, 4, 5];
 
-  const [pageList, setPageList] = useState(initPageList);
+  const [pageList, setPageList] = useState([1, 2, 3, 4, 5]);
 
-  useEffect(() => {
-    // console.log('useEffect => boardType', boardType, '/ curPage', curPage);
-    return () => setPageList([1, 2, 3, 4, 5]);
-  }, [boardType]);
-
-  const [searchModeInput, setSearchModeInput] = useState('title'); // searchModeInput
+  const [searchModeInput, setSearchModeInput] = useState('title');
   const [keywordInput, setKeywordInput] = useState('');
 
+  // 원천상태는 반드시 한 곳에서만 관리한다
   const initSearchInfo = {
     searchMode: 'title',
     keyword: '',
@@ -182,7 +167,14 @@ const Board = () => {
   const [searchInfo, setSearchInfo] = useState(initSearchInfo);
   const { keyword, searchMode } = searchInfo;
 
-  // 상태랑 아무 상관도 없음.
+  const handleClick = (pageNumber) => {
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.set('page', pageNumber);
+    console.log('searchParams', searchParams.toString());
+    history.push(`/board/${boardType}?` + searchParams.toString());
+  };
+
+  // 쿼리를 트리거(방아쇠를당기다-실행)
   const { isLoading, data } = useQuery(
     [`${boardType}Board`, curPage, sort, searchInfo],
     async () => {
@@ -197,13 +189,12 @@ const Board = () => {
       }
     }
   );
+  console.log('Board data', data);
 
-  // 얘가 setCurrentPage
   const fetchBoards = (page = 1) => {
-    // 현재 url의 searchParams
     const searchParams = new URLSearchParams(location.search);
     searchParams.set('page', page);
-    console.log('searchParams', searchParams.toString());
+    // console.log('searchParams', searchParams.toString());
     history.push(`/board/${boardType}?` + searchParams.toString());
   };
 
@@ -221,12 +212,11 @@ const Board = () => {
   };
 
   useEffect(() => {
-    if (!keyword || !searchMode) return;
+    if (keyword === '') return;
     const searchParams = new URLSearchParams(location.search);
     searchParams.set('searchMode', searchMode);
     searchParams.set('keyword', keyword);
-    console.log('searchParams', searchParams.toString());
-    setSort('createdDate');
+    // console.log('searchParams', searchParams.toString());
     history.push(`/board/${boardType}?` + searchParams.toString());
   }, [boardType, history, keyword, location.search, searchMode]);
 
@@ -236,8 +226,6 @@ const Board = () => {
     fetchBoards(1);
     const sort = dataset.name;
 
-    setSort(sort);
-
     setSearchModeInput('title');
     setKeywordInput('');
 
@@ -245,16 +233,48 @@ const Board = () => {
     history.push(`/board/${boardType}?sort=${sort}&page=1`);
   };
 
+  // 근본적인 상태 주소창을 바꾸고 react-query 요청 parcing
   const handleCancelSearch = () => {
-    fetchBoards(1);
+    console.log('Cancel ...');
+    // 궁극적으로
+    // 주소창을 바꿔야 함.
+    // searchMode와 keyword를 주소창에서 제거하도록 해야 함.
 
-    setSearchModeInput('title');
-    setKeywordInput('');
+    // page도 1로 바꿔야 함.
+    console.log(history.location);
+    const params = new URLSearchParams(history.location.search);
+    // https://developer.mozilla.org/ko/docs/Web/API/URLSearchParams?
+    params.set('page', 1);
+    params.get('searchMode') && params.delete('searchMode');
+    params.get('keyword') && params.delete('keyword');
+    console.log(`${history.location.pathname}?` + params.toString());
+    // history.push(`${history.location.pathname}?` + params.toString());
 
-    setSearchInfo(initSearchInfo);
-    history.push(`/board/${boardType}`);
+    // history push replace
+
+    // 안티패턴
+    // setSearchInfo(initSearchInfo);
+    // fetchBoards(1);
+    // setSearchModeInput('title');
+    // setKeywordInput('');
+    // // setSearchInfo(initSearchInfo);
+    // history.push(`/board/${boardType}?sort=createdDate&page=1`);
   };
 
+  // useEffect의 기능, 마운트 언마운트 lifecycle cleanup 정확히 어느 경우에 사용..?
+  // 모든 게 선언적일 수 x - side effect 부수효과
+  // 실제로html이붙음-마운트 / useEffect는 없을 수록 좋음. 부수효과 안 쓸 수 있으면
+
+  // 써야할 때는, 자기 기능대로, 관심사 별로 쪼개기
+  // useEffect(() => {
+  //   검색하고 boardType 바꾸면 url 그대로 남아있고 use-query에서 검색 api 실행됨
+  //   /board/tech?sort=createdDate&page=1&searchMode=title&keyword=%E3%85%87%E3%85%87
+  //   handleCancelSearch();
+  //   setPageList([1, 2, 3, 4, 5]);
+  //   return () => setPageList([1, 2, 3, 4, 5]);
+  // }, []);
+
+  // 선언형 프로그래밍 패러다임
   return (
     <Container>
       <Header>
@@ -305,7 +325,6 @@ const Board = () => {
           <SearchButton type="submit">
             <AiOutlineSearch className="sbtn" />
           </SearchButton>
-          {/* 폼안에 버튼 만들 때 조심하기 */}
           {keyword !== '' && (
             <CancelButton type="button" onClick={handleCancelSearch}>
               <div className="cbtn">
@@ -328,13 +347,10 @@ const Board = () => {
           ))}
           {data?.totalElements > 0 ? (
             <PageList
-              page={pageList}
-              setPage={setPageList}
-              fetchContents={fetchBoards}
-              totalPages={data?.totalPages}
-              // typeof curPage - numbers
-              currentPage={data?.currentPage}
-              sort={sort}
+              pageListState={pageList}
+              currentPageNumber={data?.currentPage}
+              endPageNumber={data?.totalPages}
+              handleClick={handleClick}
             />
           ) : (
             <div style={{ marginTop: 150, textAlign: 'center' }}>
